32fcc604c281e09d0174be62becd88d6794f2b40
aivankhirlpastor <st25480@ormiston.school.nz>
2026-08-13 13:18:24 +1200

    Implemented Add to Cart functionality

--- app.py
+++ app.py
@@ -17,15 +17,15 @@ def load_data_products():
 def index():
     return render_template("index.html")
 
-@app.route("/category/<int:m>")
+@app.route("/category/item/<int:m>")
 def product_information(m):
     models = load_data_products()
     pack_data = None
 
-    # Check whether the variable "m" matches with each of the model value,
-    # then return a valid format for the outputs.
+    # [ Check whether the variable "m" matches with each of the model value,
+    # then return a valid format for the outputs. ]
     for items, mv in models.items():
-        print(int(m) == int(mv["model"]))
+        # print(int(m) == int(mv["model"]))
         if int(m) == int(mv["model"]):
             # Product
             product_name = items
@@ -35,10 +35,55 @@ def product_information(m):
     else:
         abort(404)
 
-    print(product_name, pack_data["label"])
+    # print(product_name, pack_data["label"])
+    # Disable button of add-to-cart if product is in the cart.
+    cart = session.get("cart", {})
+    current_item = cart[product_name]
+
     # If the condition was passed, move on to prepare for the outputs.
     return render_template("product_info.html",
-                           product_name = product_name, product = pack_data)
+                           product_name = product_name, product = pack_data,
+                           already_in_cart = (product_name in cart),
+                           item_in_hold = current_item)
+
+# Add to Cart Route
+@app.route("/add_to_cart/<int:m_value><string:product_name>", methods=["POST"])
+def add_to_cart(m_value, product_name):
+    model = load_data_products()
+    quantity = int(request.form["quantity"])
+
+    # [ Check whether the variable "m" matches with each of the model value,
+    # then return a valid format for the outputs. ]
+    for items, mvt in model.items():
+        # If found and matched
+        if int(m_value) == int(mvt["model"]):            
+            break
+    else:
+        # If not...
+        return "Item not found."
+        abort(404)
+
+    # If the condition was passed, prompt the program to add items in cart.
+    cart = session.get("cart", {})
+
+    # Check whether the item is in cart already.
+    if product_name not in cart:
+        cart[product_name] = {
+            "author": model[product_name]["author"],
+            "model": model[product_name]["model"],
+            "label": model[product_name]["label"],
+            "genre": model[product_name]["genre"],
+            "price": model[product_name]["price"],
+            "quantity": quantity,
+        }
+    else:
+        raise KeyError("Item is already at the cart.")
+
+    # Update the session
+    session["cart"] = cart
+    session.modified = True
+
+    return redirect(url_for("product_information", m = m_value))
 
 @app.route("/cart")
 def cart():
